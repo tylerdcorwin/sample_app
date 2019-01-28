651d40f7d827d0c2063d7c2880f14bafd0cba897
Tyler Corwin <Tyler.C@figmints.com>
2019-01-28 16:53:00 -0500

    WIP chap10 left off at 10.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
@@ -18,9 +19,23 @@ class UsersController < ApplicationController
     end
   end
 
-
-  private def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def edit
+    @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
