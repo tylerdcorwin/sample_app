1d877cbc972f4d76d406d422f240fadbf880ed77
Tyler Corwin <Tyler.C@figmints.com>
2019-01-28 13:58:14 -0500

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  resources :users #routing for /users/1 for REST operations and SHOW's
 end
