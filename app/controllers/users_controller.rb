81c2b78396268a29b6c0fec52b225be634f8d8f8
Tyler Corwin <Tyler.C@figmints.com>
2019-01-29 09:56:28 -0500

    WIP left off at 10.2.3 friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  # Authorization to access edit and update only for logged in users
+  before_action :logged_in_user,  only: [:edit, :update]
+  before_action :correct_user,    only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -33,9 +37,20 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  private def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  private def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to login_url
     end
+  end
+
+  private def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
 end
